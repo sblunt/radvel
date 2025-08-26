399b2c3149410b34611e928dfba4fcbf3689d238
Sarah Blunt <sarah.blunt.3@gmail.com>
2025-08-26 11:02:33 -0700

    unlimited default colors (fixes plotting bugs for datasets with many insts)

--- radvel/plot/__init__.py
+++ radvel/plot/__init__.py
@@ -40,7 +40,10 @@ cmap = nipy_spectral
 rcParams['font.size'] = 9
 rcParams['lines.markersize'] = 5
 rcParams['axes.grid'] = False
-default_colors = ['orange', 'purple', 'magenta', 'pink', 'green', 'grey', 'red', 'blue', 'yellow']
+# default_colors = ['orange', 'purple', 'magenta', 'pink', 'green', 'grey', 'red', 'blue', 'yellow']
+cmap = pl.get_cmap('tab20')
+colors = [cmap(i) for i in range(cmap.N)]  # Get all colors in the colormap
+default_colors = [matplotlib.colors.rgb2hex(color) for color in colors]  # Convert to hex
 
 highlight_format = dict(marker='o', ms=16, mfc='none', mew=2, mec='gold', zorder=99)
 
